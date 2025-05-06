e19446094e9b2aad435e6b97c89a5affc887a303
EnesDemir143 <enesd41@outlook.com>
2025-05-06 12:30:17 +0300

    Text Preprocessing tamam.

--- TextPreprocessing.py
+++ TextPreprocessing.py
@@ -5,6 +5,22 @@ class TextPreprocessing:
         self.data = data
         self.is_punct = is_punct
         self.is_stop = is_stop
+        self.nlp = spacy.load('en_core_web_sm')
+
+    def _preprocess_text(self):
+        if not isinstance(self.data, str) or not self.data.strip():
+            return ""  
+        
+        text = self.data.lower()
+        doc = self.nlp(text)
+
+        filtered_data = []
+        for token in doc:
+            if (self.is_punct and token.is_punct) or ( self.is_stop and token.is_stop):
+                continue
+        filtered_data.append(token.lemma_)
+
+        return " ".join(filtered_data)
 
     def __call__(self):
-        b        
+        return self._preprocess_text()
