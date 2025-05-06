7598b1467027e18cf06f632f04c97958cf1b9465
EnesDemir143 <enesd41@outlook.com>
2025-05-07 00:46:44 +0300

    Baştan eğitilmiş bir word2vec embedding yöntemi ile model geliştirilmiştir.

--- FeatureEngineering.py
+++ FeatureEngineering.py
@@ -12,25 +12,23 @@ class FeatureEngineering:
         self.epoch = epoch
 
     def _create_vocab(self):
-        self.word2vec.build_vocab(self.data, progress_per=10000)
+        self.word2vec.build_vocab(self.data, progress_per=100)
 
     def _train_model_save(self):
         self._create_vocab()
         self.word2vec.train(self.data, epochs=self.epoch, total_examples=self.word2vec.corpus_count)
         self.word2vec.save("word2vec_emotion_data.model")
 
-    def get_document_vectors(self, texts):
-        document_vectors = []
-
-        for text in texts:
-            words_vectors = [self.word2vec.wv[word] for word in text if word in self.word2vec.wv]
-            if words_vectors:
-                doc_vector = np.mean(words_vectors, axis=0)
-            else:
-                doc_vector = np.zeros(self.word2vec.vector_size)
-            document_vectors.append(doc_vector)
-        return document_vectors
+    def get_document_vectors(self, text):
+        words_vectors = [self.word2vec.wv[word] for word in text if word in self.word2vec.wv]
+        if not words_vectors:
+            return np.zeros(self.word2vec.vector_size)
+        doc_vector = np.mean(words_vectors, axis=0)
+        return doc_vector
 
     def __call__(self):
         self._train_model_save()
         return self.word2vec
+
+if __name__ == '__main__':
+    pass
--- TextPreprocessing.py
+++ TextPreprocessing.py
@@ -1,29 +1,23 @@
 import spacy
 
 class TextPreprocessing:
-    def __init__(self, data, is_punct=None, is_stop=None):
-        self.data = data
-        self.is_punct = is_punct
-        self.is_stop = is_stop
+    def __init__(self):
         self.nlp = spacy.load('en_core_web_sm')
 
-    def _preprocess_text(self):
-        if not isinstance(self.data, str) or not self.data.strip():
+    def preprocess_text(self, data):
+        if not isinstance(data, str) or not data.strip():
             return ""  
         
-        text = self.data.lower()
+        text = data.lower()
         doc = self.nlp(text)
 
         filtered_data = []
         for token in doc:
-            if (self.is_punct and token.is_punct) or ( self.is_stop and token.is_stop):
+            if token.is_punct or token.is_stop:
                 continue
-        filtered_data.append(token.lemma_)
+            filtered_data.append(token.lemma_)
 
         return " ".join(filtered_data)
 
-    def __call__(self):
-        return self._preprocess_text()
-    
 if __name__ == '__main__':
     pass
